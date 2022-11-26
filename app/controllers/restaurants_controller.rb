95d1b376aed5407090caa73e86fe6fb2b1683c50
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-26 10:10:00 +0300

    completed code-challenge(fix): added serializer

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,7 +7,7 @@ class RestaurantsController < ApplicationController
 
     def show
         restaurant = find_restaurant
-        render json: restaurant
+        render json: restaurant, serializer: SingleRestaurantSerializer
     end
 
     def create
